44302febf0c77ea1283f1eacb7660596701ca9d7
Brad Smith <brasmith@redhat.com>
2024-11-20 14:02:46 -0500

    feat: initial CLI with placeholders for "install" and "uninstall" commands

--- quipucordsctl/main.py
+++ quipucordsctl/main.py
@@ -1,1 +1,52 @@
-print("quipucordsctl is coming soon! :)")
+"""Main command-line entrypoint."""
+
+import argparse
+
+from . import PROGRAM_NAME, SERVER_SOFTWARE_NAME
+
+
+def create_parser() -> argparse.ArgumentParser:
+    """Create the argument parser for the CLI."""
+    parser = argparse.ArgumentParser(prog=PROGRAM_NAME)
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Enable verbose output"
+    )
+    parser.add_argument(
+        "-c",
+        "--override-conf-dir",
+        dest="override_conf_dir",
+        help="Override configuration directory",
+    )
+
+    subparsers = parser.add_subparsers(dest="command")
+    # TODO load subparsers dynamically from commands package.
+
+    subparsers.add_parser("install", help=f"Install the {SERVER_SOFTWARE_NAME} server")
+    # TODO more arguments for this subparser.
+
+    subparsers.add_parser(
+        "uninstall", help=f"Uninstall the {SERVER_SOFTWARE_NAME} server"
+    )
+    # TODO more arguments for this subparser.
+
+    return parser
+
+
+def main():
+    """Run the program with arguments from the CLI."""
+    parser = create_parser()
+    args = parser.parse_args()
+
+    if args.verbose:
+        print("Verbose mode enabled.")
+
+    if args.command == "install":
+        raise NotImplementedError
+    elif args.command == "uninstall":
+        raise NotImplementedError
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
