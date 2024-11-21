827d74e1e9ba8959338ea22be219bd3b38529c5b
Brad Smith <brasmith@redhat.com>
2024-11-21 13:49:46 -0500

    feat: implement basic "install" command
    At this point, the "install" command simply copies the template files to the right places in the user's XDG directory tree and invokes the `systemctl` commands to reload the daemon to recognize the new/updated units. This basic functionality does not include any support for the "override" files. That will come later.
    
    This commit also includes placeholder code for commands "reset_django_secret" and "reset_server_password" that currently do nothing. They will come later.

---
+++ quipucordsctl/commands/reset_django_secret.py
@@ -0,0 +1,18 @@
+"""Logic for the "reset_django_secret" command."""
+
+
+def django_secret_is_set(verbose: bool = False) -> bool:
+    """Check if the Django secret key password is already set."""
+    # TODO `podman secret exists quipucords-django-secret-key`
+    return False
+
+
+def run(verbose: bool = False):
+    """Reset the server password."""
+    # TODO Implement this.
+    # TODO Should this also conditionally restart the server?
+    # Old bash installer did the following:
+    #   podman secret rm quipucords-django-secret-key >/dev/null 2>&1 || true
+    #   printf '%s' "$1" | podman secret create quipucords-django-secret-key -
+    #   podman secret ls --filter name=quipucords-django-secret-key
+    pass
---
+++ quipucordsctl/shell_utils.py
@@ -0,0 +1,29 @@
+"""Utilities for interacting with external programs."""
+
+import subprocess
+import sys
+
+
+def run_command(command: list[str]) -> tuple[str, str, int]:
+    """Run an external program."""
+    process = subprocess.Popen(
+        args=command,  # a list like ["systemctl", "--user", "reset-failed"]
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,  # we always expect input/output text, not byte strings
+    )
+
+    # TODO figure out how we want to handle stdout/stderr
+    # TODO maybe support "realtime" stdout display instead of capturing at the end
+    stdout, stderr = process.communicate()
+    exit_code = process.returncode
+
+    stdout, stderr = stdout.strip(), stderr.strip()
+    if stdout:
+        print(stdout)
+    if stderr:
+        print(stderr, file=sys.stderr)
+    if exit_code != 0:
+        raise subprocess.CalledProcessError(exit_code, command)
+
+    return stdout, stderr, exit_code
