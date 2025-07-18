bbfdc9192f8923d9283618f87a07cbf5c06cdee8
Brad Smith <brasmith@redhat.com>
2025-07-18 16:08:45 -0400

    refactor: rebuild and wrap user-facing strings for gettext l10n i18n

--- quipucordsctl/commands/reset_server_password.py
+++ quipucordsctl/commands/reset_server_password.py
@@ -1,10 +1,16 @@
-"""Reset the admin password."""
+"""Reset the server's login password."""
 
 import argparse
 import logging
+from gettext import gettext as _
 
 logger = logging.getLogger(__name__)
 NOT_A_COMMAND = True  # Until we complete the implementation.
+
+
+def get_help() -> str:
+    """Get the help/docstring for this command."""
+    return _("Reset the server's login password.")
 
 
 def server_password_is_set() -> bool:
@@ -15,7 +21,9 @@ def server_password_is_set() -> bool:
 
 def run(args: argparse.Namespace) -> None:
     """Reset the server password."""
-    logger.warning("%s is not yet implemented.", __name__)
+    logger.warning(
+        _("%(command_name)s is not yet implemented."), {"command_name": __name__}
+    )
     # TODO implement this.
     # TODO Should this also conditionally restart the server?
     # Old bash installer did the following:
