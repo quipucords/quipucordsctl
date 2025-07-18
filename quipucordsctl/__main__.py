3d5a87778de4bca85cf80a71f164bc65a09d225a
Brad Smith <brasmith@redhat.com>
2025-07-18 17:23:05 -0400

    feat: gettext install at program start

--- quipucordsctl/__main__.py
+++ quipucordsctl/__main__.py
@@ -1,6 +1,18 @@
 """Package command-line entrypoint."""
 
+import gettext
+import pathlib
+
+
+def set_up_gettext():
+    """Set up gettext for string localization."""
+    locale_dir = pathlib.Path(__file__).parent / "locale"
+    gettext.bindtextdomain("messages", str(locale_dir))
+
+
 if __name__ == "__main__":
+    set_up_gettext()
+
     from .main import main
 
     main()
