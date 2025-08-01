quipucords/quipucordsctl

commit c563ba71403c97b6be0ac9dc891fffda9fb35ffd

refactor: rename reset_server_password to reset_admin_password

quipucordsctl/commands/install.py
@@ -7,7 +7,7 @@ import shutil
 from gettext import gettext as _
 
 from .. import settings, shell_utils
-from . import reset_django_secret, reset_server_password
+from . import reset_admin_password, reset_django_secret
 
 DATA_DIRS = ("data", "db", "log", "sshkeys")
 SYSTEMCTL_USER_RESET_FAILED_CMD = ["systemctl", "--user", "reset-failed"]
@@ -100,8 +100,8 @@ def run(args: argparse.Namespace) -> None:
     if args.override_conf_dir:
         raise NotImplementedError
 
-    if not reset_server_password.server_password_is_set():
-        reset_server_password.run(args)
+    if not reset_admin_password.server_password_is_set():
+        reset_admin_password.run(args)
     if not reset_django_secret.django_secret_is_set():
         reset_django_secret.run(args)
 

tests/commands/test_install.py
@@ -39,11 +39,11 @@ def test_install_run(temp_config_directories: dict[str, pathlib.Path]):
     systemd_dir = temp_config_directories["SYSTEMD_UNITS_DIR"]
     with (
         mock.patch.object(install, "reset_django_secret") as reset_django_secret,
-        mock.patch.object(install, "reset_server_password") as reset_server_password,
+        mock.patch.object(install, "reset_admin_password") as reset_admin_password,
         mock.patch.object(install, "systemctl_reload") as systemctl_reload,
     ):
         reset_django_secret.django_secret_is_set.return_value = False
-        reset_server_password.server_password_is_set.return_value = False
+        reset_admin_password.server_password_is_set.return_value = False
 
         install.run(mock_args)
 
@@ -54,7 +54,7 @@ def test_install_run(temp_config_directories: dict[str, pathlib.Path]):
 
         systemctl_reload.assert_called_once()
         reset_django_secret.run.assert_called_once()
-        reset_server_password.run.assert_called_once()
+        reset_admin_password.run.assert_called_once()
 
 
 def test_systemctl_reload(mock_shell_utils):

tests/commands/test_reset_admin_password.py
@@ -1,0 +1,17 @@
+"""Test the "reset_admin_password" command."""
+
+from unittest import mock
+
+# TODO FIXME Implement the rest of these tests.
+from quipucordsctl.commands import reset_admin_password
+
+
+def test_server_password_is_set():
+    """Test placeholder for django_secret_is_set."""
+    assert not reset_admin_password.server_password_is_set()
+
+
+def test_reset_django_secret_run():
+    """Test placeholder for reset_admin_password.run."""
+    mock_args = mock.Mock()
+    assert reset_admin_password.run(mock_args) is None

tests/commands/test_reset_server_password.py
@@ -1,17 +1,0 @@
-"""Test the "reset_server_password" command."""
-
-from unittest import mock
-
-# TODO FIXME Implement the rest of these tests.
-from quipucordsctl.commands import reset_server_password
-
-
-def test_server_password_is_set():
-    """Test placeholder for django_secret_is_set."""
-    assert not reset_server_password.server_password_is_set()
-
-
-def test_reset_django_secret_run():
-    """Test placeholder for reset_server_password.run."""
-    mock_args = mock.Mock()
-    assert reset_server_password.run(mock_args) is None

commit 32959ed365bd1f8d50d08b517699187442a9d433

fix: rename server_password_is_set to admin_password_is_set (missed in/caused by c563ba7)

quipucordsctl/commands/install.py
@@ -100,7 +100,7 @@ def run(args: argparse.Namespace) -> None:
     if args.override_conf_dir:
         raise NotImplementedError
 
-    if not reset_admin_password.server_password_is_set():
+    if not reset_admin_password.admin_password_is_set():
         reset_admin_password.run(args)
     if not reset_application_secret.application_secret_is_set():
         reset_application_secret.run(args)

tests/commands/test_install.py
@@ -45,7 +45,7 @@ def test_install_run(temp_config_directories: dict[str, pathlib.Path]):
         mock.patch.object(install, "systemctl_reload") as systemctl_reload,
     ):
         reset_application_secret.application_secret_is_set.return_value = False
-        reset_admin_password.server_password_is_set.return_value = False
+        reset_admin_password.admin_password_is_set.return_value = False
 
         install.run(mock_args)
 