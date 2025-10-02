a852263b0eb8fdf406f77d6911754f2ad7562f33
Brad Smith <brasmith@redhat.com>
2025-10-02 17:00:57 -0400

    feat: read env vars to set secret values
    Relates to JIRA: DISCOVERY-1124

--- src/quipucordsctl/commands/reset_session_secret.py
+++ src/quipucordsctl/commands/reset_session_secret.py
@@ -14,6 +14,7 @@ from quipucordsctl import podman_utils, secrets, settings, shell_utils
 
 logger = logging.getLogger(__name__)
 SESSION_SECRET_PODMAN_SECRET_NAME = settings.QUIPUCORDS_SECRETS["session"]  # noqa: S105
+SESSION_SECRET_ENV_VAR_NAME = f"{settings.ENV_VAR_PREFIX}SESSION_SECRET_KEY"
 SECRET_MIN_LENGTH = 64
 
 
@@ -40,6 +41,37 @@ def is_set() -> bool:
     return podman_utils.secret_exists(SESSION_SECRET_PODMAN_SECRET_NAME)
 
 
+def confirm_replace_existing_secret() -> bool:
+    """Confirm that the user wants to replace an existing secret."""
+    logger.warning(
+        _(
+            "The encryption secret key already exists. "
+            "Resetting the encryption secret key to a new value "
+            "may result in data loss if you have already installed "
+            "and run %(SERVER_SOFTWARE_NAME)s on this system."
+        ),
+        {"SERVER_SOFTWARE_NAME": settings.SERVER_SOFTWARE_NAME},
+    )
+    return shell_utils.confirm(
+        _("Are you sure you want to replace the existing database password? [y/n] ")
+    )
+
+
+def confirm_manual_reset() -> bool:
+    """Confirm that the user wants to manually set a value into this secret."""
+    logger.warning(
+        _(
+            "You should only manually reset the encryption secret key "
+            "if you understand how it it used and you are addressing a specific "
+            "issue. We strongly recommend using the automatically generated "
+            "encryption secret key instead of manually entering one."
+        )
+    )
+    return shell_utils.confirm(
+        _("Are you sure you want to manually reset the encryption secret key? [y/n] ")
+    )
+
+
 def run(args: argparse.Namespace) -> bool:
     """
     Reset the server session secret key.
@@ -50,17 +82,7 @@ def run(args: argparse.Namespace) -> bool:
     * Return True if everything succeeds, or False if user declines any prompt.
     """
     if getattr(args, "prompt", False):
-        logger.warning(
-            _(
-                "You should only manually reset the session secret key if you "
-                "understand how it it used and you are addressing a specific issue. "
-                "We strongly recommend using the automatically generated session "
-                "secret key instead of manually entering one."
-            )
-        )
-        if not shell_utils.confirm(
-            _("Are you sure you want to manually reset the session secret key? [y/n] ")
-        ):
+        if not confirm_manual_reset():
             return False
         if not (
             new_secret := secrets.prompt_secret(
@@ -70,6 +92,17 @@ def run(args: argparse.Namespace) -> bool:
             logger.error(_("The session secret key was not updated."))
             return False
     else:
+        new_secret, invalid = secrets.read_from_env(
+            SESSION_SECRET_ENV_VAR_NAME,
+            _("Redis password"),
+            min_length=SECRET_MIN_LENGTH,
+        )
+        if invalid or (new_secret and not confirm_manual_reset()):
+            # Early return because env var was found but failed checks.
+            # Or valid env var found and user declined to use it.
+            return False
+
+    if not new_secret:
         new_secret = secrets.generate_random_secret(SECRET_MIN_LENGTH)
         logger.info(
             _(
--- tests/commands/test_reset_database_password.py
+++ tests/commands/test_reset_database_password.py
@@ -1,5 +1,6 @@
 """Test the "reset_database_password" command."""
 
+import argparse
 import logging
 from unittest import mock
 
@@ -20,7 +21,9 @@ def test_database_password_is_set(mock_secret_exists):
 @mock.patch.object(reset_database_password.shell_utils, "confirm")
 @mock.patch.object(reset_database_password.secrets, "generate_random_secret")
 @mock.patch.object(reset_database_password.secrets, "prompt_secret")
+@mock.patch.object(reset_database_password.secrets, "read_from_env")
 def test_reset_database_password_run_fails_set_secret(  # noqa: PLR0913
+    mock_read_from_env,
     mock_prompt_secret,
     mock_generate_random_secret,
     mock_confirm,
@@ -31,13 +34,15 @@ def test_reset_database_password_run_fails_set_secret(  # noqa: PLR0913
 ):
     """Test reset_database_password.run when set_secret fails unexpectedly."""
     caplog.set_level(logging.ERROR)
-    mock_args = mock.Mock()
-    mock_args.prompt = False
+    args = argparse.Namespace()
+    args.prompt = False
     mock_secret_exists.return_value = False  # act like first-time setup
     mock_generate_random_secret.return_value = good_secret
     mock_set_secret.return_value = False  # might happen in a race condition
+    mock_read_from_env.return_value = None, False
 
-    assert not reset_database_password.run(mock_args)
+    assert not reset_database_password.run(args)
+    mock_read_from_env.assert_called_once()
     mock_secret_exists.assert_called_once()
     mock_set_secret.assert_called_once()
     mock_prompt_secret.assert_not_called()  # no prompts for default first-time setup
@@ -50,7 +55,9 @@ def test_reset_database_password_run_fails_set_secret(  # noqa: PLR0913
 @mock.patch.object(reset_database_password.shell_utils, "confirm")
 @mock.patch.object(reset_database_password.secrets, "generate_random_secret")
 @mock.patch.object(reset_database_password.secrets, "prompt_secret")
+@mock.patch.object(reset_database_password.secrets, "read_from_env")
 def test_reset_database_password_run_success(  # noqa: PLR0913
+    mock_read_from_env,
     mock_prompt_secret,
     mock_generate_random_secret,
     mock_confirm,
@@ -59,13 +66,15 @@ def test_reset_database_password_run_success(  # noqa: PLR0913
     good_secret,
 ):
     """Test reset_database_password.run is successful with default (no prompt) args."""
-    mock_args = mock.Mock()
-    mock_args.prompt = False
+    args = argparse.Namespace()
+    args.prompt = False
     mock_secret_exists.return_value = False  # act like first-time setup
     mock_generate_random_secret.return_value = good_secret
     mock_set_secret.return_value = True
+    mock_read_from_env.return_value = None, False
 
-    assert reset_database_password.run(mock_args)
+    assert reset_database_password.run(args)
+    mock_read_from_env.assert_called_once()
     mock_secret_exists.assert_called_once_with(
         reset_database_password.DATABASE_PASSWORD_PODMAN_SECRET_NAME
     )
@@ -82,7 +91,9 @@ def test_reset_database_password_run_success(  # noqa: PLR0913
 @mock.patch.object(reset_database_password.podman_utils, "secret_exists")
 @mock.patch.object(reset_database_password.shell_utils, "confirm")
 @mock.patch.object(reset_database_password.secrets, "prompt_secret")
+@mock.patch.object(reset_database_password.secrets, "read_from_env")
 def test_reset_database_password_run_with_prompts_success(  # noqa: PLR0913
+    mock_read_from_env,
     mock_prompt_password,
     mock_confirm,
     mock_secret_exists,
@@ -90,14 +101,15 @@ def test_reset_database_password_run_with_prompts_success(  # noqa: PLR0913
     good_secret,
 ):
     """Test reset_database_password.run is successful with user input prompts."""
-    mock_args = mock.Mock()
-    mock_args.prompt = True  # require user to confirm
+    args = argparse.Namespace()
+    args.prompt = True  # require user to confirm
     mock_confirm.return_value = True  # act like use always enters 'y'
     mock_secret_exists.return_value = True
     mock_set_secret.return_value = True
     mock_prompt_password.return_value = good_secret
 
-    assert reset_database_password.run(mock_args)
+    assert reset_database_password.run(args)
+    mock_read_from_env.assert_not_called()  # because user asked to be prompted
     mock_secret_exists.assert_called_once()
     mock_prompt_password.assert_called_once()
     mock_set_secret.assert_called_once_with(
@@ -112,7 +124,9 @@ def test_reset_database_password_run_with_prompts_success(  # noqa: PLR0913
 @mock.patch.object(reset_database_password.podman_utils, "secret_exists")
 @mock.patch.object(reset_database_password.shell_utils, "confirm")
 @mock.patch.object(reset_database_password.secrets, "prompt_secret")
+@mock.patch.object(reset_database_password.secrets, "read_from_env")
 def test_reset_database_password_run_decline_replace_existing(  # noqa: PLR0913
+    mock_read_from_env,
     mock_prompt_password,
     mock_confirm,
     mock_secret_exists,
@@ -124,6 +138,7 @@ def test_reset_database_password_run_decline_replace_existing(  # noqa: PLR0913
     mock_confirm.return_value = False  # act like use always enters 'n'
 
     assert not reset_database_password.run(mock_args)
+    mock_read_from_env.assert_not_called()  # because user asked to be prompted
     mock_secret_exists.assert_called_once()
     mock_prompt_password.assert_not_called()
     mock_set_secret.assert_not_called()
@@ -134,7 +149,9 @@ def test_reset_database_password_run_decline_replace_existing(  # noqa: PLR0913
 @mock.patch.object(reset_database_password.podman_utils, "secret_exists")
 @mock.patch.object(reset_database_password.shell_utils, "confirm")
 @mock.patch.object(reset_database_password.secrets, "prompt_secret")
+@mock.patch.object(reset_database_password.secrets, "read_from_env")
 def test_reset_database_password_run_decline_manual_input(  # noqa: PLR0913
+    mock_read_from_env,
     mock_prompt_password,
     mock_confirm,
     mock_secret_exists,
@@ -146,7 +163,40 @@ def test_reset_database_password_run_decline_manual_input(  # noqa: PLR0913
     mock_confirm.side_effect = [True, False]  # act like use enters 'y' then 'n'
 
     assert not reset_database_password.run(mock_args)
+    mock_read_from_env.assert_not_called()  # because user asked to be prompted
     mock_secret_exists.assert_called_once()
     mock_prompt_password.assert_not_called()
     mock_set_secret.assert_not_called()
     assert len(mock_confirm.call_args_list) == 2
+
+
+@mock.patch.object(reset_database_password.podman_utils, "set_secret")
+@mock.patch.object(reset_database_password.podman_utils, "secret_exists")
+@mock.patch.object(reset_database_password.shell_utils, "settings")
+@mock.patch.object(reset_database_password.secrets, "prompt_secret")
+@mock.patch.object(reset_database_password.secrets, "read_from_env")
+def test_reset_database_password_headless_mode(  # noqa: PLR0913
+    mock_read_from_env,
+    mock_prompt_password,
+    mock_settings,
+    mock_secret_exists,
+    mock_set_secret,
+    good_secret,
+):
+    """Test reset_database_password.run using env vars, yes, and quiet."""
+    args = argparse.Namespace()
+    mock_settings.yes = True
+    mock_settings.quiet = True
+    mock_secret_exists.return_value = True
+    mock_set_secret.return_value = True
+    mock_read_from_env.return_value = good_secret, False
+
+    assert reset_database_password.run(args)
+    mock_read_from_env.assert_called_once()
+    mock_secret_exists.assert_called_once()
+    mock_prompt_password.assert_not_called()
+    mock_set_secret.assert_called_once_with(
+        reset_database_password.DATABASE_PASSWORD_PODMAN_SECRET_NAME,
+        good_secret,
+        True,
+    )
