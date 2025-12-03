30eba98f7a95d601e945f88cd7e94ac9fc37229f
abellotti <abellott@redhat.com>
2025-12-03 12:16:48 -0500

    feat: add support for environment vars in run_command
    - Add support for environment vars in run_command so we
      can pass on to popen. This is necessary if we need
      to setup things like LANG=C and the like before running
      programs.

--- src/quipucordsctl/shell_utils.py
+++ src/quipucordsctl/shell_utils.py
@@ -77,7 +77,8 @@ def run_command(
     raise_error: bool = True,
     wait_timeout: int | None = None,
     stdin: str | None = None,
-) -> tuple[str, str, int]:
+    env: dict[str, str] | None = None,
+) -> tuple[str, str, int]:  # sourcery skip
     """Run an external program."""
     if not all(isinstance(arg, str) for arg in command):
         raise TypeError(_("Command arguments must be strings. Got: %r") % command)
@@ -89,13 +90,14 @@ def run_command(
             {"wait_timeout": wait_timeout},
         )
     try:
-        process = subprocess.Popen(
+        process = subprocess.Popen(  # sourcery skip
             args=command,  # a list like ["systemctl", "--user", "reset-failed"]
             stdin=subprocess.PIPE if stdin else subprocess.DEVNULL,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,  # we always expect input/output text, not byte strings
             shell=False,  # redundant, but a safe precaution in case defaults change
+            env=env,
         )
 
         # TODO figure out how we want to handle stdout/stderr
